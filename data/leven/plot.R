1ebe17c20891433ce996da18e17916cd68503625
Caio Raposo <caioraposo@disroot.org>
2022-07-21 15:18:50 -0300

    Add levenshtein distance

---
+++ data/leven/plot.R
@@ -0,0 +1,11 @@
+f1 <- read.csv(file = "lev-memo.csv")
+f2 <- read.csv(file = "lev-hand-memo.csv")
+
+f1$time = f1$time
+f2$time = f2$time
+
+plot(f1$n, f1$time, xlab = "n", ylab = "Time (miliseconds)", pch = 19, frame = FALSE, col = "blue")
+points(f2$n, f2$time, pch = 19, col = "red")
+
+
+legend(1, 1500, legend=c("Builtin memoization", "Handcrafted memoization"), pch=c(19, 19), col=c("blue", "red"))
