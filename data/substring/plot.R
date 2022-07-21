46e3cdb22cd2fe4a607c1181e8fd939a8cb0f20b
Caio Raposo <caioraposo@disroot.org>
2022-07-21 20:08:11 -0300

    Use a different string for each execution

--- data/substring/plot.R
+++ data/substring/plot.R
@@ -8,4 +8,4 @@ plot(f1$n, f1$time, xlab = "n", ylab = "Time (miliseconds)", pch = 19, frame = F
 points(f2$n, f2$time, pch = 19, col = "red")
 
 
-legend(2, 200, legend=c("Builtin memoization", "Handcrafted memoization"), pch=c(19, 19), col=c("blue", "red"))
+legend(5, 24, legend=c("Builtin memoization", "Handcrafted memoization"), pch=c(19, 19), col=c("blue", "red"))
