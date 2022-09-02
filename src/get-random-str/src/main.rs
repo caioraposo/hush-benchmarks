18b2f4120b5b708490fa99da95330df0f7a5c511
Caio Raposo <caioraposo@disroot.org>
2022-09-02 19:31:30 -0300

    Add string generator written in Rust to replace the current Python one

---
+++ src/get-random-str/src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use rand::Rng;
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	let n: usize = args[1].parse().unwrap();
+    let mut rng = rand::thread_rng();
+
+    let mut chars: Vec<u8> = vec![0; n];
+    for el in &mut chars {
+        let mut ch = rng.gen_range(65..117);
+        if ch > 90 {
+            ch += 6;
+        }
+        *el = ch;
+    }
+
+    let rand_str = String::from_utf8(chars).unwrap();
+    print!("{}", rand_str);
+}
+
